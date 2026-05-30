18f4457081e4391c664d42328053ab82fb019593
otani-dental-kakogawa <ryotanikako@gmail.com>
2026-05-31 00:54:44 +0900

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('[data-menu]')?.addEventListener('click',()=>document.getElementById('drawer').classList.toggle('open'));
-window.addEventListener('scroll',()=>document.getElementById('header').classList.toggle('scrolled',window.scrollY>20));
